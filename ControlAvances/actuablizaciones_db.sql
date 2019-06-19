db592c6b22023b2b763e812c9a654a972f84b218
JULIAN <JULIAN@DESKTOP-JON05L4>
2019-06-19 06:37:41 -0500

    Egresos EPS listo

---
+++ ControlAvances/actuablizaciones_db.sql
@@ -0,0 +1,2 @@
+INSERT INTO `menu_submenus` (`ID`, `Nombre`, `idPestana`, `idCarpeta`, `idMenu`, `TablaAsociada`, `TipoLink`, `JavaScript`, `Pagina`, `Target`, `Estado`, `Image`, `Orden`, `Updated`, `Sync`) VALUES
+(14,	'Cargar Egresos',	12,	5,	2,	'',	1,	'',	'cargar_egresos.php',	'_SELF',	1,	'egresoitems.png',	4,	'2019-06-07 19:16:15',	'0000-00-00 00:00:00');
